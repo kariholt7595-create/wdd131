ef605aeaa4c08730e800a9ee7787374a9be07abd
kariholt7595-create <kholt1@byupathway.edu>
2026-05-16 12:38:07 -0600

    temple pic album first pass

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("show");
+    hamButton.classList.toggle("show");
+});
