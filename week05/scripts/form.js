b21195de38b15d9012128454189797974620d4fb
kariholt7595-create <kariholt7595@gmail.com>
2026-06-05 22:57:34 -0600

    form first pass

---
+++ week05/scripts/form.js
@@ -0,0 +1,68 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+const currentYear = document.getElementById("currentyear");
+
+if (currentYear) {
+    currentYear.textContent = year;
+}
+
+const lastModified = document.getElementById("lastModified");
+
+if (lastModified) {
+    lastModified.innerHTML = document.lastModified;
+}
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productSelect = document.querySelector("#productName");
+
+if (productSelect) {
+    products.forEach(product => {
+        const option = document.createElement("option");
+
+        option.textContent = product.name;
+        option.value = product.id;
+
+        productSelect.appendChild(option);
+    });
+}
+
+const reviewCountSpan = document.querySelector("#reviewCount");
+
+if (reviewCountSpan) {
+    let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+    reviewCount++;
+
+    localStorage.setItem("reviewCount", reviewCount);
+
+    reviewCountSpan.textContent = reviewCount;
+}
